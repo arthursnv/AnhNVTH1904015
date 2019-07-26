2b9dcdc5bbe8bd4ea40505fb83b011dfcb488866
thienan0789 <38343906+thienan0789@users.noreply.github.com>
2019-07-26 11:23:54 +0700

    Added Demo4 and modify Demo3

--- Data Types/Demo3.cpp
+++ Data Types/Demo3.cpp
@@ -2,9 +2,36 @@
 
 int main(){
 	int n;
+	char c = 'c';
 	
 	printf("Nhap vao so bat ky: ");
 	scanf("%d", &n);
+	
+	switch(n){
+		case 2:
+			printf("Day la Thu hai\n");
+			break;
+		case 3:
+			printf("Day la Thu ba\n");
+			break;
+		case 4:
+			printf("Day la Thu tu\n");
+			break;
+		case 5:
+			printf("Day la Thu nam\n");
+			break;
+		case 6:
+			printf("Day la Thu sau\n");
+			break;
+		case 7:
+			printf("Day la Thu bay\n");
+			break;
+		case 8:
+			printf("Day la Chu nhat\n");
+			break;
+		default: 
+			printf("Day khong phai ngay trong tuan.\n");
+	}
 	
 	if(1 < n && n < 9){
 		if(n==8){
---
+++ Data Types/Demo4.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a, b, c;
+	int p = 0;
+	int s = 0;
+	
+	printf("Nhap vao do dai 3 canh de kiem tra: ");
+	printf("\nCanh thu nhat: ");
+	scanf("%d", &a);
+	printf("\nCanh thu hai: ");
+	scanf("%d", &b);
+	printf("\nCanh thu ba: ");
+	scanf("%d", &c);
+	
+	if((a+b)>c && (a+c)>b && (b+a)>c && (b+c)>a && (c+a)>b && (c+b)>a){
+		printf("\nDay la 1 tam giac");
+		p = a + b + c;
+		printf("\n- Chu vi = %d", p);
+		int p2 = p/2;
+		s = sqrt(p2*(p2-a)*(p2-b)*(p2-c));
+		printf("\n- Dien tich = %d", s);
+	} else {
+		printf("\nDay khong phai la 1 tam giac");
+	}
+	
+//	if((a+b)>c){
+//		if((a+c)>b){
+//			if((b+a)>c){
+//				if((b+c)>a){
+//					if((c+a)>b){
+//						if((c+b)>a){
+//							printf("\nDay la 1 tam giac");
+//							p = a + b + c;
+//							printf("\n- Chu vi = %d", p);
+//							int p2 = p/2;
+//							s = sqrt(p2*(p2-a)*(p2-b)*(p2-c));
+//							printf("\n- Dien tich = %d", s);
+//						} else {
+//							printf("\nDay khong phai la 1 tam giac");
+//						}
+//					} else {
+//						printf("\nDay khong phai la 1 tam giac");
+//					}
+//				} else {
+//					printf("\nDay khong phai la 1 tam giac");
+//				}
+//			} else {
+//				printf("\nDay khong phai la 1 tam giac");
+//			}
+//		} else {
+//			printf("\nDay khong phai la 1 tam giac");
+//		}
+//	} else {
+//		printf("\nDay khong phai la 1 tam giac");
+//	}
+}
