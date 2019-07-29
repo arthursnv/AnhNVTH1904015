b4e1e3924fa143215fa7796d3a8b773583129eab
thienan0789 <38343906+thienan0789@users.noreply.github.com>
2019-07-29 19:02:32 +0700

    Update

--- LBEP/Bai3_Assignment4.cpp
+++ LBEP/Bai3_Assignment4.cpp
@@ -12,10 +12,10 @@ int main(){
 	int m = n;
 	int temp = 0;
 	do{
-		temp = temp * 10 + n % 10;
-		n /= 10;
-	} while (n > 0);
-	if(temp==m){
+		temp = temp * 10 + m % 10;
+		m /= 10;
+	} while (m > 0);
+	if(temp==n){
 		printf("\nDay la so thuan nghich.");
 	} else {
 		printf("\nDay khong phai la so thuan nghich.");
