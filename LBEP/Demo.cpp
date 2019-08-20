2dc9d2e79af4f96763e2edb16755f19590c22e06
thienan0789 <38343906+thienan0789@users.noreply.github.com>
2019-08-20 13:43:42 +0700

    Added Web Front-end folder

---
+++ LBEP/Demo.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char a[2000];
+	char b[2000];
+	
+	printf("Chuoi 1: "); gets(a);
+	printf("Chuoi 2: "); gets(b);
+	
+	int status;
+	status == strcmp(a, b);
+	
+	printf("Trang thai: %d", status);
+	
+}
